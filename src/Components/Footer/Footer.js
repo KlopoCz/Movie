a132252faf690318f814b4b0757f60866fe1ebe4
Tomas <vrana.tomik@email.cz>
2021-01-23 17:11:37 +0100

    adding github link unfinshed

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./footer.css";
 
 const Footer = () => {
-  return <div className="FooterContiner"></div>;
+  return (
+    <div className="FooterContiner">
+      <div className="LogoFooter">
+        <h1>site made with</h1>
+        <span></span>
+      </div>
+      <div className="GitHubFooter">
+        <span></span>
+        <h1>GitHub</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
