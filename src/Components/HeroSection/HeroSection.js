c85852de8c823899e8020ff02efa32b54017dd01
Tomas <klopo877@seznam.cz>
2021-02-07 11:29:55 +0100

    position next button + bug fix

--- src/Components/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection.js
@@ -31,7 +31,7 @@ const HeroSection = () => {
             setImg(data.backdrop_path);
             setValue(value + 1);
             setRadius(((2 * 3.141592654 * 25) / 100) * data.vote_average * 10);
-
+            setSaved(Plus);
             Object.keys(localStorage).forEach(function (key) {
               if (key === data.original_title) {
                 setSaved(Checked);
@@ -41,7 +41,7 @@ const HeroSection = () => {
       });
   }, []);
   const add = () => {
-    if (value === 6) {
+    if (value === 19) {
       setValue(0);
     } else {
       setValue(value + 1);
@@ -53,7 +53,12 @@ const HeroSection = () => {
       .then((data) => {
         setBestMovieDet(data);
         setImg(data.backdrop_path);
-
+        setSaved(Plus);
+        Object.keys(localStorage).forEach(function (key) {
+          if (key === data.original_title) {
+            setSaved(Checked);
+          }
+        });
         setRadius(((2 * 3.141592654 * 25) / 100) * data.vote_average * 10);
       });
   };
@@ -75,116 +80,118 @@ const HeroSection = () => {
         <div>
           <div
             className="BlackFade"
-            // onClick={add}
             style={{
               backgroundImage: `url(https://image.tmdb.org/t/p/w1280/${bestMovieDet.backdrop_path}) `,
             }}
           >
-            <div className="HeroPageContentContainer">
-              <h1 className="title">
-                {bestMovieDet.original_title} (
-                {bestMovieDet.release_date.substring(0, 4)})
-              </h1>
-              <h1 className="year"></h1>
-              <div className="SVGcontainer">
-                <SvgCircle
-                  radius={radius}
-                  vote_average={bestMovieDet.vote_average}
-                  SvgClass={"circle-chart"}
-                ></SvgCircle>
-                <h1 className="score">User Score </h1>
-              </div>
-              <p className="overview">{bestMovieDet.overview}</p>
+            <div className="contantcontainer">
+              <div className="HeroPageContentContainer">
+                <h1 className="title">
+                  {bestMovieDet.original_title} (
+                  {bestMovieDet.release_date.substring(0, 4)})
+                </h1>
+                <h1 className="year"></h1>
+                <div className="SVGcontainer">
+                  <SvgCircle
+                    radius={radius}
+                    vote_average={bestMovieDet.vote_average}
+                    SvgClass={"circle-chart"}
+                  ></SvgCircle>
+                  <h1 className="score">User Score </h1>
+                </div>
+                <p className="overview">{bestMovieDet.overview}</p>
 
-              <p className="tagline"> {bestMovieDet.tagline}</p>
-              <div className="ButtonsContainer">
-                <div className="AddContainer" onClick={addToFavourite}>
-                  <span style={{ backgroundImage: `url(${saved})` }}></span>
-                  <h1>Add</h1>
+                <p className="tagline"> {bestMovieDet.tagline}</p>
+                <div className="ButtonsContainer">
+                  <div className="AddContainer" onClick={addToFavourite}>
+                    <span style={{ backgroundImage: `url(${saved})` }}></span>
+                    <h1>Add</h1>
+                  </div>
+                  <div className="PlayContainer">
+                    <span></span>
+                    <h1>
+                      <a href={`/movie?q=${bestMovieDet.id}`}>See More</a>
+                    </h1>
+                  </div>
                 </div>
-                <div className="PlayContainer">
-                  <span></span>
-                  <h1>
-                    <a href={`/movie?q=${bestMovieDet.id}`}>See More</a>
-                  </h1>
-                </div>
+                <ul className="InfoContainer">
+                  <li>
+                    <h2>Relised:</h2>
+                    <h1>{bestMovieDet.release_date}</h1>
+                  </li>
+                  <li>
+                    <h2>Time:</h2>
+                    <h1>
+                      {Math.floor(bestMovieDet.runtime / 60)}h
+                      {bestMovieDet.runtime % 60}m
+                    </h1>
+                  </li>
+                  <li>
+                    <h2>Company:</h2>
+                    <h1>
+                      {bestMovieDet.production_companies.length !== 0
+                        ? bestMovieDet.production_companies[0].name
+                        : "Sorry but no"}
+                    </h1>
+                  </li>
+                </ul>
+                <ul className="GenersContainer">
+                  {bestMovieDet.genres.length > 0 ? (
+                    <div>
+                      <li>{bestMovieDet.genres[0].name}</li>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {bestMovieDet.genres.length > 1 ? (
+                    <div>
+                      <li> - </li>
+                      <li>{bestMovieDet.genres[1].name}</li>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {bestMovieDet.genres.length > 2 ? (
+                    <div>
+                      <li> - </li>
+                      <li>{bestMovieDet.genres[2].name}</li>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {bestMovieDet.genres.length > 3 ? (
+                    <div>
+                      <li> - </li>
+                      <li>{bestMovieDet.genres[3].name}</li>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {bestMovieDet.genres.length > 4 ? (
+                    <div>
+                      <li> - </li>
+                      <li>{bestMovieDet.genres[4].name}</li>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                </ul>
               </div>
-              <ul className="InfoContainer">
-                <li>
-                  <h2>Relised:</h2>
-                  <h1>{bestMovieDet.release_date}</h1>
-                </li>
-                <li>
-                  <h2>Time:</h2>
-                  <h1>
-                    {Math.floor(bestMovieDet.runtime / 60)}h
-                    {bestMovieDet.runtime % 60}m
-                  </h1>
-                </li>
-                <li>
-                  <h2>Company:</h2>
-                  <h1>
-                    {bestMovieDet.production_companies.length !== 0
-                      ? bestMovieDet.production_companies[0].name
-                      : "Sorry but no"}
-                  </h1>
-                </li>
-              </ul>
-              <ul className="GenersContainer">
-                {bestMovieDet.genres.length > 0 ? (
-                  <div>
-                    <li>{bestMovieDet.genres[0].name}</li>
-                  </div>
-                ) : (
-                  ""
-                )}
-                {bestMovieDet.genres.length > 1 ? (
-                  <div>
-                    <li> - </li>
-                    <li>{bestMovieDet.genres[1].name}</li>
-                  </div>
-                ) : (
-                  ""
-                )}
-                {bestMovieDet.genres.length > 2 ? (
-                  <div>
-                    <li> - </li>
-                    <li>{bestMovieDet.genres[2].name}</li>
-                  </div>
-                ) : (
-                  ""
-                )}
-                {bestMovieDet.genres.length > 3 ? (
-                  <div>
-                    <li> - </li>
-                    <li>{bestMovieDet.genres[3].name}</li>
-                  </div>
-                ) : (
-                  ""
-                )}
-                {bestMovieDet.genres.length > 4 ? (
-                  <div>
-                    <li> - </li>
-                    <li>{bestMovieDet.genres[4].name}</li>
-                  </div>
-                ) : (
-                  ""
-                )}
-              </ul>
+              <a className="HeroPageImgA" href={`/movie?q=${bestMovieDet.id}`}>
+                <div
+                  style={{
+                    backgroundImage: `url(https://image.tmdb.org/t/p/w400/${bestMovieDet.poster_path}) `,
+                  }}
+                  className="HeroPageImg"
+                ></div>
+              </a>{" "}
             </div>
-            <a href={`/movie?q=${bestMovieDet.id}`}>
-              <div
-                style={{
-                  backgroundImage: `url(https://image.tmdb.org/t/p/w400/${bestMovieDet.poster_path}) `,
-                }}
-                className="HeroPageImg"
-              ></div>
-            </a>
-          </div>
+            <div className="NextButton" onClick={add}></div>
+          </div>{" "}
         </div>
       ) : (
         <h3 className="LodingText">Loding...</h3>
-      )}
+      )}{" "}
     </div>
   );
 };
